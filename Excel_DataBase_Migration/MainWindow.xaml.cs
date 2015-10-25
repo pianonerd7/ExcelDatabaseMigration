d4167641d4eabb3d64c7c48b633d7d4d1d0f8ef5
pianonerd7 <jxh604@case.edu>
2015-10-25 00:04:11 -0400

    binding from options to config works!!! WOOT

--- Excel_DataBase_Migration/MainWindow.xaml.cs
+++ Excel_DataBase_Migration/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Excel_Database_Migration.ViewModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Excel_Database_Migration
 {
@@ -22,6 +10,7 @@ namespace Excel_Database_Migration
     {
         public MainWindow()
         {
+            DataContext = new OptionsViewModel(this);
             InitializeComponent();
         }
     }
