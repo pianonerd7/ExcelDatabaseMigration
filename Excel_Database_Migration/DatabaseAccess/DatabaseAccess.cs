06451cd6b94f47a9e7ddff17c34fd738bd5eb266
pianonerd7 <jxh604@case.edu>
2015-10-25 16:13:26 -0400

    dbaccess class done

---
+++ Excel_Database_Migration/DatabaseAccess/DatabaseAccess.cs
@@ -0,0 +1,93 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Excel_Database_Migration.DatabaseAccess
+{
+    protected class DatabaseAccess
+    {
+
+        private readonly string _connectionString;
+        private const string CON_STRING = "will get filled after i make the other UI...";
+        
+        
+        /// <summary>
+        /// Default Constructor for the Database class. 
+        /// Automatically connects to Microsoft SQL Server on current computer.
+        /// </summary>
+        public DatabaseAccess() : this(CON_STRING) 
+        {
+  
+        } 
+
+        /// <summary>
+        /// Constructor overload in case user want's to connect to another server.
+        /// </summary>
+        /// <param name="connectionString"></param>
+        public DatabaseAccess(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        /// <summary>
+        /// Establish and open connection with SQL server.
+        /// </summary>
+        /// <returns></returns>
+        private static SqlConnection ConnectToSql() {
+
+            SqlConnection connection = new SqlConnection(CON_STRING);
+            connection.Open();
+            return connection;
+
+        }
+
+        /// <summary>
+        /// Responsible for Select queries. 
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns>Return a datatable of queries results</returns>
+        protected static DataTable GetQuery(string query) {
+
+            using (SqlConnection connection = ConnectToSql()) {
+                using (SqlDataAdapter adapter = new SqlDataAdapter()) {
+
+                    try {
+                        SqlCommand command = new SqlCommand(query, connection);
+                        DataTable queryResult = new DataTable();
+
+                        adapter.SelectCommand = command;
+                        adapter.Fill(queryResult);
+
+                        return queryResult;
+                    }
+
+                    finally {
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Responsible for Delete, Insert, and Update queries.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns>Return the number of rows affected</returns>
+        protected static int GetNonQuery(string query) {
+
+            int nonQuery = -1;
+
+            using (SqlConnection connection = ConnectToSql()) {
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                try {
+                    nonQuery = command.ExecuteNonQuery();
+                }
+
+                finally {  
+                }
+
+                return nonQuery;
+            }
+        }
+    }
+}
