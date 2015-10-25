59da467f8afa4cdb5e18887d0cf7baf38f8d7e8c
pianonerd7 <jxh604@case.edu>
2015-10-24 23:34:15 -0400

    added VM base

---
+++ Excel_Database_Migration/ViewModel/ViewModelBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel_Database_Migration.ViewModel
+{
+ public class ViewModelBase : INotifyPropertyChanged
+    {
+
+        #region Private Properties
+
+
+
+        #endregion
+
+        #region Public Properties
+
+    
+        #endregion
+
+        #region INotifyProperty
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void OnPropertyChanged(string propertyName)
+        {
+
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
