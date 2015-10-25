fc82b7bded0cc6c01c32dfbd7e2628fd1914a4a1
pianonerd7 <jxh604@case.edu>
2015-10-24 23:42:50 -0400

    migrationvm about done

--- Excel_Database_Migration/ViewModel/MigrationDataConfigViewModel.cs
+++ Excel_Database_Migration/ViewModel/MigrationDataConfigViewModel.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Excel_Database_Migration.ViewModel
 {
-    class MigrationDataConfigViewModel
+    class MigrationDataConfigViewModel : ViewModelBase
     {
+
+        #region Private Declarations
+
+        private readonly ICommand _selectFilePathCommand;
+
+        #endregion
+
+        #region Constructor
+
+        public MigrationDataConfigViewModel()
+        {
+            _selectFilePathCommand = new DelegateCommand(ExecuteSelectFilePathCommand, CanExecuteCommand);
+        }
+
+        #endregion 
+
+        #region Public Properties
+
+        public ICommand SelectFilePathCommand
+        {
+            get
+            {
+                return _selectFilePathCommand;
+            }
+        }
+        #endregion
+
+        #region Private Methods
+
+        private bool CanExecuteCommand(object obj)
+        {
+            return true;
+        }
+
+        private void ExecuteSelectFilePathCommand(object obj)
+        {
+
+        }
+
+        #endregion
+
     }
 }
---
+++ Excel_Database_Migration/ViewModel/OptionsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Excel_Database_Migration.ViewModel
+{
+    class OptionsViewModel : ViewModelBase
+    {
+
+        #region Private Declaration
+
+        private readonly ICommand _migrateDataCommand;
+        private readonly ICommand _accessDatabaseCommand;
+
+        #endregion
+
+    }
+}
