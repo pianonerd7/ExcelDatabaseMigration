d13b1f230cf5ff41ec5eb2d4ead842b0c8c4a88a
Ted Kim <tkk21@case.edu>
2015-10-28 22:45:57 -0400

    implemented createSchema and createInsert

--- GenerateSQL/SQLGeneration/SQLBuilder.cs
+++ GenerateSQL/SQLGeneration/SQLBuilder.cs
@@ -25,7 +25,7 @@ namespace GenerateSQL.SQLGeneration
 
         public SQLBuilder createSchema()
         {
-            builder.Append(String.Format("{0}", schemaName));
+            builder.Append(String.Format("CREATE SCHEMA IF NOT EXISTS {0};\n", schemaName));
             return this;
         }
 
@@ -41,14 +41,66 @@ namespace GenerateSQL.SQLGeneration
                 builder.Append(", ");
                 }
             }
-            builder.Append(")");
+            builder.Append(");\n");
             return this;
         }
 
+        /**
+        *Writes the SQL Statement for all the rows of data
+        */
         public SQLBuilder createInsert()
         {
-            builder.Append(String.Format("INSERT "));
+            string attributes = formatAttributes();
+            for (int i = 0; i<csv.Data.Count; i++)
+            {
+                createInsertRow(attributes, i);
+            }
             return this;
+        }
+
+        /*
+        *Writes the SQL Statement for one row insert
+        *
+        *Insert Statement in SQL looks like
+        *INSERT INTO table_name (attributes) VALUES(values);
+        */
+        private SQLBuilder createInsertRow(String attributes, int index)
+        {
+            builder.Append(String.Format("INSERT INTO {0}(", tableName));
+            builder.Append(attributes);
+            builder.Append("VALUES (");
+            
+            for (int j = 0; j<csv.Data[index].Length; j++)
+            {
+                builder.Append(csv.Data[index][j]);
+                if (j != csv.Data[index].Length - 1)
+                {
+                    builder.Append(", ");
+                }
+            }
+            builder.Append(");\n");
+            return this;
+        }
+
+        private String formatAttributes()
+        {
+            StringBuilder attributeBuilder = new StringBuilder();
+            attributeBuilder.Append("(");
+            for (int i = 0; i < csv.Attributes.Length; i++)
+            {
+                attributeBuilder.Append(csv.Attributes[i]);
+                if (i != csv.Attributes.Length - 1)
+                {
+                    attributeBuilder.Append(", ");
+                }
+            }
+            attributeBuilder.Append(")");
+            return attributeBuilder.ToString();
+        }
+
+        public string build()
+        {
+            return builder.ToString();
         }
     }
 }
