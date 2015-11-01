76c029148563a02884a205d0465c253a30eaf7c3
pianonerd7 <jxh604@case.edu>
2015-11-01 15:33:52 -0500

    changed namespace

--- Excel_DataBase_Migration_Test/TestHelpers/FormatPath.cs
+++ Excel_DataBase_Migration_Test/TestHelpers/FormatPath.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExcelDatabaseMigrationTest.TestHelpers
+namespace Excel_DataBase_Migration_Test.TestHelpers
 {
     class FormatPath
     {
