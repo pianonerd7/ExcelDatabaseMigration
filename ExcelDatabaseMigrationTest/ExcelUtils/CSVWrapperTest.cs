9488e0c61c20e1c95c12907f344c9d4e3b6c69e6
Ted Kim <tkk21@case.edu>
2015-11-01 13:53:56 -0500

    CSVWrapperTest implemented

--- ExcelDatabaseMigrationTest/ExcelUtils/CSVWrapperTest.cs
+++ ExcelDatabaseMigrationTest/ExcelUtils/CSVWrapperTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Excel_Database_Migration.ExcelUtils;
+using System.IO;
+using System.Diagnostics;
 
 namespace ExcelDatabaseMigrationTest.ExcelUtils
 {
@@ -8,9 +10,72 @@ namespace ExcelDatabaseMigrationTest.ExcelUtils
     public class CSVWrapperTest
     {
         [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
         public void openCSV_emptyPath()
         {
-            
+            CSVWrapper csv = new CSVWrapper();
+            csv.openCSV("");
         }
+
+        [TestMethod]
+        public void directory()
+        {
+            Trace.WriteLine("environment is: " + Environment.CurrentDirectory);
+            Trace.WriteLine("environment is: " + formatPath("test1.csv"));
+        }
+
+        [TestMethod]
+        public void openCSV_existing_EmptyFile()
+        {
+            CSVWrapper csv = new CSVWrapper();
+            csv.openCSV(formatPath("empty.csv"));
+            Assert.IsNull(csv.Attributes);
+        }
+
+        [TestMethod]
+        public void openCSV_existing_smallFile()
+        {
+            CSVWrapper csv = new CSVWrapper();
+            csv.openCSV(formatPath("test1.csv"));
+            Assert.IsNotNull(csv.Attributes);
+            string[] expectedAttributes = {"Name", "Gender", "Salary" };
+            checkAttributes(expectedAttributes, csv);
+
+            string[][] expectedData = { new string[] { "Tom", "M", "20" },
+            new string[] {"Adam", "M", "30" },
+            new string[] {"Sara", "F", "40" },
+            new string[] {"Serena", "F", "50" } };
+            checkData(expectedData, csv);
+        }
+
+        private string formatPath (string filename)
+        {
+            DirectoryInfo d = new DirectoryInfo(Environment.CurrentDirectory);
+            Trace.WriteLine(d.ToString());
+            
+            DirectoryInfo twoup = d.Parent.Parent;
+            Trace.WriteLine(twoup.FullName.ToString());
+            return twoup.FullName.ToString() + "\\" + "testData" + "\\" + filename;
+        }
+
+        private void checkAttributes(String[] expected, CSVWrapper csv)
+        {
+            for (int i = 0; i < csv.Attributes.Length; i++)
+            {
+                Assert.AreEqual(expected[i], csv.Attributes[i]);
+            }
+        }
+
+        private void checkData (String[][] expected, CSVWrapper csv)
+        {
+            for (int i = 0; i<csv.Data.Count; i++)
+            {
+                for (int j = 0; j<csv.Data[i].Length; j++)
+                {
+                    Assert.AreEqual(expected[i][j], csv.Data[i][j]);
+                }
+            }
+        }
+        
     }
 }
