0e8c2e1da1b77c75f6d4126f9fe7f42ecc30384a
pianonerd7 <jxh604@case.edu>
2015-11-10 18:15:46 -0500

    created dbaccess VM

---
+++ Excel_Database_Migration/ViewModel/DatabaseAccessViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel_Database_Migration.ViewModel
+{
+    public class DatabaseAccessViewModel : ViewModelBase
+    {
+
+        public DatabaseAccessViewModel()
+        {
+
+        }
+    }
+}
