22671a39da867319cf673651eca9f076569aeead
pianonerd7 <jxh604@case.edu>
2015-11-10 23:19:04 -0500

    created dataprovider class

--- Excel_Database_Migration/ViewModel/DatabaseAccessViewModel.cs
+++ Excel_Database_Migration/ViewModel/DatabaseAccessViewModel.cs
@@ -1,17 +1,37 @@
-﻿using System;
+﻿using Excel_Database_Migration.ExcelUtils;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Excel_Database_Migration.ViewModel
 {
     public class DatabaseAccessViewModel : ViewModelBase
     {
+         #region Private Declaration
 
-        public DatabaseAccessViewModel()
+        private Window _mainWindow;
+
+        #endregion
+
+        #region Constructor
+        public DatabaseAccessViewModel(Window window)
+        {
+            this._mainWindow = window;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void populateGrid(DataTable dataList)
         {
 
         }
+
+        #endregion
     }
 }
