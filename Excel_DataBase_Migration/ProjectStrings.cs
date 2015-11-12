645db02a91b99282d36a993606daf01aa87d01cb
Ted Kim <tkk21@case.edu>
2015-11-11 21:29:44 -0500

    Externalize strings to reduce coupling

---
+++ Excel_DataBase_Migration/ProjectStrings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel_Database_Migration
+{
+    static class ProjectStrings
+    {
+        public const string APPLICATION_NAME = "Excel Database Migration";
+    }
+}
