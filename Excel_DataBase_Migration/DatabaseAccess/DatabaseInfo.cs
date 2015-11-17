d02af141987116fb20234e8d3edbec420c4f7c4d
Ted Kim <tkk21@case.edu>
2015-11-17 18:21:31 -0500

    created a wrapper static class to hold connection string and name

---
+++ Excel_DataBase_Migration/DatabaseAccess/DatabaseInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel_Database_Migration.DatabaseAccess
+{
+    public static class DatabaseInfo
+    {
+        private static string connectionString;
+        public static string ConnectionString {
+            get { return connectionString; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException();
+                }
+                connectionString = value;
+            }
+        }
+
+        private static string databaseName;
+        public static string DatabaseName
+        {
+            get { return databaseName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException();
+                }
+                databaseName = value;
+            }
+        }
+
+
+    }
+}
