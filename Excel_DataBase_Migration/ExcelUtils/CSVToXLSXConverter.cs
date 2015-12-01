6c5d428bc3148ae4a667176cb3bc99c9cd31748f
Ted <tkk21@case.edu>
2015-11-30 19:32:05 -0500

    convert csv to xlsx

---
+++ Excel_DataBase_Migration/ExcelUtils/CSVToXLSXConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel_Database_Migration.ExcelUtils
+{
+    public class CSVToXLSXConverter
+    {
+        public static void toXLSX(string filePath, string destination)
+        {
+            Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook wb = app.Workbooks.Open(filePath, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+            // this does not throw exception if file doesnt exist
+            File.Delete(destination);
+
+            wb.SaveAs(destination, Microsoft.Office.Interop.Excel.XlFileFormat.xlXMLSpreadsheet, Type.Missing, Type.Missing, false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Microsoft.Office.Interop.Excel.XlSaveConflictResolution.xlLocalSessionChanges, false, Type.Missing, Type.Missing, Type.Missing);
+
+            wb.Close(false, Type.Missing, Type.Missing);
+
+            app.Quit();
+        }
+    }
+}
