ad17b215fa4ae82ec6f772da8f25b4f4777afe49
Ted <tkk21@case.edu>
2015-12-03 13:51:41 -0500

    unit test for data table converter

---
+++ Excel_DataBase_Migration_Test/ExcelUtils/DataTableToCSVConverterTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Excel_Database_Migration.ExcelUtils;
+using System.Data;
+
+namespace Excel_Database_Migration_Test.ExcelUtils
+{
+    [TestClass]
+    public class DataTableToCSVConverterTest
+    {
+        [TestMethod]
+        public void convert_Empty()
+        {
+            DataTable table;
+            DataTableToCSVConverter.ConvertToCSV(table);
+        }
+    }
+}
