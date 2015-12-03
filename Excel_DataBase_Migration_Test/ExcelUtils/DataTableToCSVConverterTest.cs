728943017fbfe330ba2c873bd2d150c5bc5129ea
Ted <tkk21@case.edu>
2015-12-03 14:09:08 -0500

    some test for data table to csv converter

--- Excel_DataBase_Migration_Test/ExcelUtils/DataTableToCSVConverterTest.cs
+++ Excel_DataBase_Migration_Test/ExcelUtils/DataTableToCSVConverterTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Excel_Database_Migration.ExcelUtils;
 using System.Data;
+using System.Diagnostics;
 
 namespace Excel_Database_Migration_Test.ExcelUtils
 {
@@ -11,8 +12,32 @@ namespace Excel_Database_Migration_Test.ExcelUtils
         [TestMethod]
         public void convert_Empty()
         {
-            DataTable table;
-            DataTableToCSVConverter.ConvertToCSV(table);
+            DataTable table = new DataTable();
+            CSVWrapper csv =  DataTableToCSVConverter.ConvertToCSV(table);
+            Assert.AreEqual(0, csv.Attributes.Length);
+            Assert.AreEqual(0, csv.Data.Count);
+        }
+
+        [TestMethod]
+        public void convert_small()
+        {
+            DataTable table = new DataTable();
+            table.Clear();
+            table.Columns.Add("Name");
+            table.Columns.Add("Age");
+            DataRow row1 = table.NewRow();
+            row1["Name"] = "ted";
+            row1["Age"] = "21";
+            table.Rows.Add(row1);
+            CSVWrapper csv = DataTableToCSVConverter.ConvertToCSV(table);
+            Assert.AreEqual(2, csv.Attributes.Length);
+            Assert.AreEqual(1, csv.Data.Count);
+
+            Assert.AreEqual("Name", csv.Attributes[0]);
+            Assert.AreEqual("Age", csv.Attributes[1]);
+            Assert.AreEqual("ted", csv.Data[0][0]);
+            Assert.AreEqual("21", csv.Data[0][1]);
+
         }
     }
 }
